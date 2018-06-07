5fc3d608397225064188c8eeced83830acd193cf
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-06-08 00:25:04 +0300

    AssemblyInfo update.

--- SacredUtils/Properties/AssemblyInfo.cs
+++ SacredUtils/Properties/AssemblyInfo.cs
@@ -50,8 +50,8 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.3.5")]
-[assembly: AssemblyFileVersion("1.2.3.5")]
+[assembly: AssemblyVersion("1.2.3.6")]
+[assembly: AssemblyFileVersion("1.2.3.6")]
 [assembly: Guid("CDAA45BE-EA1A-41BF-A36A-6F14199667E1")]
 [assembly: NeutralResourcesLanguage("ru")]
 
