2f56c2e22eb7f2b57e1105f3a632f51f22a45a4d
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-08-19 22:51:38 +0300

    Added ability switching category.

--- SacredUtils/MainWindow.xaml.cs
+++ SacredUtils/MainWindow.xaml.cs
@@ -23,23 +23,50 @@ namespace SacredUtils
         {
             InitializeComponent();
 
+            application_settings_one appStgOne = new application_settings_one();
+            application_settings_two appStgTwo = new application_settings_two();
+            chat_settings_one chatStgOne = new chat_settings_one();
+            font_settings_one fontStgOne = new font_settings_one();
+            gameplay_settings_one gameplayStgOne = new gameplay_settings_one();
+            gameplay_settings_two gameplayStgTwo = new gameplay_settings_two();
+            gameplay_settings_three gameplayStgThree = new gameplay_settings_three();
+            graphics_settings_one graphicsStgOne = new graphics_settings_one();
+            graphics_settings_two graphicsStgTwo = new graphics_settings_two();
+            graphics_settings_three graphicsStgThree = new graphics_settings_three();
+            graphics_settings_four graphicsStgFour = new graphics_settings_four();
+            modify_settings_one modifyStgOne = new modify_settings_one();
+            sound_settings_one soundStgOne = new sound_settings_one();
+            unselected_settings_one unselectedStgOne = new unselected_settings_one();
+
+            GraphicsPanel.MouseLeftButtonDown += (s, e) => SelectSettings(graphicsStgOne);
+            SoundPanel.MouseLeftButtonDown += (s, e) => SelectSettings(soundStgOne);
+            GameplayPanel.MouseLeftButtonDown += (s, e) => SelectSettings(gameplayStgOne);
+            ChatPanel.MouseLeftButtonDown += (s, e) => SelectSettings(chatStgOne);
+            FontsPanel.MouseLeftButtonDown += (s, e) => SelectSettings(fontStgOne);
+            ModifPanel.MouseLeftButtonDown += (s, e) => SelectSettings(modifyStgOne);
+            SettingsPanel.MouseLeftButtonDown += (s, e) => SelectSettings(appStgOne);
+
             CloseBtn.Click += (s, e) => Application.Current.Shutdown();
             MinimizeBtn.Click += (s, e) => WindowState = WindowState.Minimized;
+
             ToolPanel.MouseDown += DragWindow;
 
+            SelectSettings(unselectedStgOne);
 
-            application_settings_two page = new application_settings_two();
-            SettingsFrame.Content = page;
-
-            Height = Height * 1.2;
-            Width = Width * 1.2;
-            BaseCard.LayoutTransform = new ScaleTransform(1.2, 1.2);
-            SettingsBorder.BorderThickness = new Thickness(0,1,1,0);
+//            Height = Height * 1.2;
+//            Width = Width * 1.2;
+//            BaseCard.LayoutTransform = new ScaleTransform(1.2, 1.2);
+//            SettingsBorder.BorderThickness = new Thickness(0,1,1,0);
         }
 
         public void DragWindow(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left) { DragMove(); }
+        }
+
+        public void SelectSettings(UIElement element)
+        {
+            SettingsFrame.Content = element;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
