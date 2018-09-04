4797124795f130d1491490e215f16db80c07a875
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-04 18:46:23 +0300

    Removed unused directives.

--- SacredUtils/resources/pgs/application_settings_two.xaml.cs
+++ SacredUtils/resources/pgs/application_settings_two.xaml.cs
@@ -1,24 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace SacredUtils.resources.pgs
+﻿namespace SacredUtils.resources.pgs
 {
-    /// <summary>
-    /// Логика взаимодействия для application_settings_two.xaml
-    /// </summary>
-    public partial class application_settings_two : Page
+    // ReSharper disable once InconsistentNaming
+    public partial class application_settings_two
     {
         public application_settings_two()
         {
--- SacredUtils/resources/pgs/unselected_settings_one.xaml.cs
+++ SacredUtils/resources/pgs/unselected_settings_one.xaml.cs
@@ -1,24 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace SacredUtils.resources.pgs
+﻿namespace SacredUtils.resources.pgs
 {
-    /// <summary>
-    /// Логика взаимодействия для unselected_settings_one.xaml
-    /// </summary>
-    public partial class unselected_settings_one : Page
+    // ReSharper disable once InconsistentNaming
+    public partial class unselected_settings_one
     {
         public unselected_settings_one()
         {
