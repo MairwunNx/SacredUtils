98660727e8e66692500539075f18591b6abbc8c4
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 20:32:06 +0300

    Hotkey dialog converted to page.

---
+++ SacredUtils/resources/dlg/hotkey_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class hotkey_dialog
+    {
+        public hotkey_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
