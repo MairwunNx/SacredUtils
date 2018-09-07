80e9e78a2c0650531c45321d4a0b2de67ae15c72
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 20:19:52 +0300

    Restoring success dialog converted to page.

---
+++ SacredUtils/resources/dlg/restoring_success_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class restoring_success_dialog
+    {
+        public restoring_success_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
