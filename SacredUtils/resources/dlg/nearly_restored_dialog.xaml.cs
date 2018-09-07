0668e7f1d755bda86d55d64a77dac4ef32c865f0
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 20:09:42 +0300

    Nearly restored dialog converted to page.

---
+++ SacredUtils/resources/dlg/nearly_restored_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class nearly_restored_dialog
+    {
+        public nearly_restored_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
