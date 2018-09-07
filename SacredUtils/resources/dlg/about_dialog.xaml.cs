c9786a6a42cab3d8d3893cf509cb0b7a593275ee
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 20:03:53 +0300

    About dialog converted to page.

---
+++ SacredUtils/resources/dlg/about_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class about_dialog
+    {
+        public about_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
