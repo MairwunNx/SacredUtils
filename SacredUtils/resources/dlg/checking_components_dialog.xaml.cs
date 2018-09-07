e16920a7c0cd4c5770e9d6306cf0d540e2c1d23e
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 20:27:29 +0300

    Checking components dialog converted to page.

---
+++ SacredUtils/resources/dlg/checking_components_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class checking_components_dialog
+    {
+        public checking_components_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
