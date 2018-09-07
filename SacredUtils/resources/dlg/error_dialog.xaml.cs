69d7e8d0eb4da266a21aea3e89e57d6232e3efb9
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 19:59:30 +0300

    Error dialog converted to page.

---
+++ SacredUtils/resources/dlg/error_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class error_dialog
+    {
+        public error_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
