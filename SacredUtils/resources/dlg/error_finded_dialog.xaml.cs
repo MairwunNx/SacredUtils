2022b318bda4fbcfc0be6774c146aff9067f554a
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 20:13:20 +0300

    Error finded dialog converted to page.

---
+++ SacredUtils/resources/dlg/error_finded_dialog.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class error_finded_dialog : Page
+    {
+        public error_finded_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
