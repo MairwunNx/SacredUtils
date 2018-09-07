7dee54f276f5369de16cce29e60707c0a5e42cf1
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 20:34:52 +0300

    Get components dialog converted to page.

---
+++ SacredUtils/resources/dlg/get_components_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class get_components_dialog
+    {
+        public get_components_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
