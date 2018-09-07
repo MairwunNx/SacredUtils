de124a16e4da04379bbe5fcc27f2faef61bbe31e
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-07 20:23:02 +0300

    Error not found dialog converted to page.

---
+++ SacredUtils/resources/dlg/error_not_found_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class error_not_found_dialog
+    {
+        public error_not_found_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
