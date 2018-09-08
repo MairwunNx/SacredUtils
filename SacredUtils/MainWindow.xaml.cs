6f242eb0cab64f2af786c1be4f79c5879250d89a
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-08 19:28:22 +0300

    Added selecting settings func on buttons.

--- SacredUtils/MainWindow.xaml.cs
+++ SacredUtils/MainWindow.xaml.cs
@@ -27,28 +27,22 @@ namespace SacredUtils
         {
             InitializeComponent();
 
-            application_settings_one appStgOne = new application_settings_one();
-            application_settings_two appStgTwo = new application_settings_two();
-            chat_settings_one chatStgOne = new chat_settings_one();
-            font_settings_one fontStgOne = new font_settings_one();
-            gameplay_settings_one gameplayStgOne = new gameplay_settings_one();
-            gameplay_settings_two gameplayStgTwo = new gameplay_settings_two();
-            gameplay_settings_three gameplayStgThree = new gameplay_settings_three();
-            graphics_settings_one graphicsStgOne = new graphics_settings_one();
-            graphics_settings_two graphicsStgTwo = new graphics_settings_two();
-            graphics_settings_three graphicsStgThree = new graphics_settings_three();
-            graphics_settings_four graphicsStgFour = new graphics_settings_four();
-            modify_settings_one modifyStgOne = new modify_settings_one();
-            sound_settings_one soundStgOne = new sound_settings_one();
+            application_settings_one appStgOne       = new application_settings_one();
+            chat_settings_one chatStgOne             = new chat_settings_one();
+            font_settings_one fontStgOne             = new font_settings_one();
+            gameplay_settings_one gameplayStgOne     = new gameplay_settings_one();
+            graphics_settings_one graphicsStgOne     = new graphics_settings_one();
+            modify_settings_one modifyStgOne         = new modify_settings_one();
+            sound_settings_one soundStgOne           = new sound_settings_one();
             unselected_settings_one unselectedStgOne = new unselected_settings_one();
 
-            GraphicsPanel.MouseLeftButtonDown += (s, e) => SelectSettings(graphicsStgOne);
-            SoundPanel.MouseLeftButtonDown += (s, e) => SelectSettings(soundStgOne);
-            GameplayPanel.MouseLeftButtonDown += (s, e) => SelectSettings(gameplayStgOne);
-            ChatPanel.MouseLeftButtonDown += (s, e) => SelectSettings(chatStgOne);
-            FontsPanel.MouseLeftButtonDown += (s, e) => SelectSettings(fontStgOne);
-            ModifPanel.MouseLeftButtonDown += (s, e) => SelectSettings(modifyStgOne);
-            SettingsPanel.MouseLeftButtonDown += (s, e) => SelectSettings(appStgOne);
+            MenuGrLabel.Click += (s, e) => SelectSettings(graphicsStgOne);
+            MenuSnLabel.Click += (s, e) => SelectSettings(soundStgOne);
+            MenuGpLabel.Click += (s, e) => SelectSettings(gameplayStgOne);
+            MenuCtLabel.Click += (s, e) => SelectSettings(chatStgOne);
+            MenuFtLabel.Click += (s, e) => SelectSettings(fontStgOne);
+            MenuMdLabel.Click += (s, e) => SelectSettings(modifyStgOne);
+            MenuStLabel.Click += (s, e) => SelectSettings(appStgOne);
 
             CloseBtn.Click += (s, e) => Application.Current.Shutdown();
             MinimizeBtn.Click += (s, e) => WindowState = WindowState.Minimized;
@@ -57,9 +51,9 @@ namespace SacredUtils
 
             SelectSettings(unselectedStgOne);
 
-            Height = Height * 1.2;
-            Width = Width * 1.2;
-            BaseCard.LayoutTransform = new ScaleTransform(1.2, 1.2);
+//            Height = Height * 1.2;
+//            Width = Width * 1.2;
+//            BaseCard.LayoutTransform = new ScaleTransform(1.2, 1.2);
 
             GlobalizedApplication.Instance.GlobalizationManager.SwitchLanguage
             (ApplicationInfo.Lang == "ru" ? "ru-RU" : "en-US", true);
@@ -79,7 +73,7 @@ namespace SacredUtils
         {
             SettingsFrame.Content = element;
 
-            GlobalizedApplication.Instance.StyleManager.SwitchStyle("Dark.xaml");
+//            GlobalizedApplication.Instance.StyleManager.SwitchStyle("Dark.xaml");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
