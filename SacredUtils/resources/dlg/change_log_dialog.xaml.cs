616a37bf970ea30932bab64d0a668def43fd36a1
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-09-09 18:08:31 +0300

    Added change log dialog.

---
+++ SacredUtils/resources/dlg/change_log_dialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SacredUtils.resources.dlg
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class change_log_dialog
+    {
+        public change_log_dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
