27b6234d5c524629e5f11b007a5ae9f001976c70
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-10-08 12:11:46 +0300

    Ability to load game font settings done.

--- SacredUtils/resources/pgs/font_settings_one.xaml.cs
+++ SacredUtils/resources/pgs/font_settings_one.xaml.cs
@@ -1,11 +1,47 @@
-﻿namespace SacredUtils.resources.pgs
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SacredUtils.resources.pgs
 {
     // ReSharper disable once InconsistentNaming
     public partial class font_settings_one
     {
         public font_settings_one()
         {
-            InitializeComponent();
+            InitializeComponent(); GetSettings();
+        }
+
+        private void GetSettings()
+        {
+            List<string> fonts = new InstalledFontCollection().Families.Select(font => font.Name).ToList();
+
+            FontNameCmbBox.ItemsSource = fonts; fonts.Remove("");
+
+            var text = File.ReadAllLines("Settings.cfg", Encoding.ASCII);
+
+            for (var i = 0; i < text.Length; i++)
+            {
+                if (text[i].Contains("FONT : 1, "))
+                {
+                    var tempdata0 = text[i].Substring(text[i].IndexOf("\"", StringComparison.Ordinal) + 1);
+
+                    if (tempdata0 == text[i].Substring(text[i].IndexOf("\"", StringComparison.Ordinal) + 1))
+                    {
+                        var tempdata1 = tempdata0.Remove(tempdata0.LastIndexOf("\"", StringComparison.Ordinal));
+
+                        FontNameCmbBox.SelectedItem = tempdata1;
+                    }
+                }
+            }
+        }
+
+        private void EventSubscribe()
+        {
+            // todo: добавить возможность изменять значения настроек.
         }
     }
 }
