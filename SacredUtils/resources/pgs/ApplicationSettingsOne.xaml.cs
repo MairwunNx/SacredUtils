05de146d2860f3d5e6df059ff284c4497457e034
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-10-11 12:30:44 +0300

    Finally ended work on application settings one.

--- SacredUtils/resources/pgs/ApplicationSettingsOne.xaml.cs
+++ SacredUtils/resources/pgs/ApplicationSettingsOne.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using MaterialDesignThemes.Wpf;
 using SacredUtils.resources.dlg;
-using System;
+using SacredUtils.resources.prp;
 using System.Diagnostics;
-using System.Globalization;
 using System.Windows;
-using System.Windows.Media;
-using WPFSharp.Globalizer;
 
 namespace SacredUtils.resources.pgs
 {
@@ -13,38 +10,13 @@ namespace SacredUtils.resources.pgs
     {
         public ApplicationSettingsOne()
         {
-            InitializeComponent(); GetSettings();
+            InitializeComponent(); EventSubscribe(); DataContext = new ApplicationSettingsOneProperty(); 
 
             AppLogger.Log.Info("Initialization components for application settings one done!");
         }
 
-        private void GetSettings()
-        {
-            if (AppSettings.ApplicationSettings.AppUiLanguage == "based on system")
-            {
-                UiLanguageCmbBox.SelectedIndex = CultureInfo.InstalledUICulture.TwoLetterISOLanguageName == "ru" ? 0 : 1;
-            }
-            else
-            {
-                UiLanguageCmbBox.SelectedIndex = AppSettings.ApplicationSettings.AppUiLanguage == "ru" ? 0 : 1;
-            }
-
-            UiThemeCmbBox.SelectedIndex = AppSettings.ApplicationSettings.ColorTheme == "light" ? 0 : 1;
-
-            StartParamsCmbBox.SelectedIndex = AppSettings.ApplicationSettings.SacredStartArgs;
-
-            UiScaleCmbBox.Text = $"{Convert.ToInt32(AppSettings.ApplicationSettings.SacredUtilsGuiScale * 100)}%";
-
-            EventSubscribe();
-        }
-
         private void EventSubscribe()
         {
-            UiLanguageCmbBox.SelectionChanged += (s, e) => ChangeLanguage();
-            UiThemeCmbBox.SelectionChanged += (s, e) => ChangeTheme();
-            StartParamsCmbBox.SelectionChanged += (s, e) => ChangeStart();
-            UiScaleCmbBox.SelectionChanged += (s, e) => ChangeScale();
-
             GitHubBtn.Click += (s, e) => OpenLink("https://github.com/MairwunNx/SacredUtils");
             DonateBtn.Click += (s, e) => OpenDonateSelectDialog();
             CreatorBtn.Click += (s, e) => OpenPageSelectDialog();
@@ -52,108 +24,6 @@ namespace SacredUtils.resources.pgs
             AboutBtn.Click += (s, e) => OpenAboutDialog();
 
             ToTwoPageBtn.Click += (s, e) => OpenTwoPage();
-        }
-
-        private void ChangeLanguage()
-        {
-            if (UiLanguageCmbBox.SelectedIndex == 0)
-            {
-                AppSettings.ApplicationSettings.AppUiLanguage = "ru";
-
-                AppLogger.Log.Info("SacredUtils application language changed state to ru by user");
-            }
-
-            if (UiLanguageCmbBox.SelectedIndex == 1)
-            {
-                AppSettings.ApplicationSettings.AppUiLanguage = "en";
-
-                AppLogger.Log.Info("SacredUtils application language changed state to en by user");
-            }
-
-            GlobalizedApplication.Instance.GlobalizationManager.SwitchLanguage
-                (UiLanguageCmbBox.SelectedIndex == 0 ? "ru-RU" : "en-US", true);
-        }
-
-        private void ChangeTheme()
-        {
-            AppSettings.ApplicationSettings.ColorTheme = UiThemeCmbBox.SelectedIndex == 0 ? "light" : "dark";
-
-            GlobalizedApplication.Instance.StyleManager.SwitchStyle
-                (UiThemeCmbBox.SelectedIndex == 0 ? "Light.xaml" : "Dark.xaml");
-
-            AppLogger.Log.Info(UiThemeCmbBox.SelectedIndex == 0
-                ? "SacredUtils application theme changed state to light by user"
-                : "SacredUtils application theme changed state to dark by user");
-        }
-
-        private void ChangeStart()
-        {
-            AppSettings.ApplicationSettings.SacredStartArgs = StartParamsCmbBox.SelectedIndex;
-
-            AppLogger.Log.Info($"Sacred game startup params changed to {StartParamsCmbBox.SelectedIndex} by user");
-        }
-
-        private void ChangeScale()
-        {
-            if (UiScaleCmbBox.SelectedIndex == 0)
-            {
-                ChangeScale(1.0);
-                AppLogger.Log.Info("SacredUtils Ui scale changed state to 100% by user");
-            }
-
-            if (UiScaleCmbBox.SelectedIndex == 1)
-            {
-                ChangeScale(1.05);
-                AppLogger.Log.Info("SacredUtils Ui scale changed state to 105% by user");
-            }
-
-            if (UiScaleCmbBox.SelectedIndex == 2)
-            {
-                ChangeScale(1.10);
-                AppLogger.Log.Info("SacredUtils Ui scale changed state to 110% by user");
-            }
-
-            if (UiScaleCmbBox.SelectedIndex == 3)
-            {
-                ChangeScale(1.15);
-                AppLogger.Log.Info("SacredUtils Ui scale changed state to 115% by user");
-            }
-
-            if (UiScaleCmbBox.SelectedIndex == 4)
-            {
-                ChangeScale(1.25);
-                AppLogger.Log.Info("SacredUtils Ui scale changed state to 125% by user");
-            }
-
-            if (UiScaleCmbBox.SelectedIndex == 5)
-            {
-                ChangeScale(1.50);
-                AppLogger.Log.Info("SacredUtils Ui scale changed state to 150% by user");
-            }
-
-            if (UiScaleCmbBox.SelectedIndex == 6)
-            {
-                ChangeScale(1.75);
-                AppLogger.Log.Info("SacredUtils Ui scale changed state to 175% by user");
-            }
-
-            if (UiScaleCmbBox.SelectedIndex == 7)
-            {
-                ChangeScale(2.0);
-                AppLogger.Log.Info("SacredUtils Ui scale changed state to 200% by user");
-            }
-        }
-
-        private static void ChangeScale(double scale)
-        {
-            foreach (Window window in Application.Current.Windows)
-            {
-                ((MainWindow)window).Height = 720 * scale;
-                ((MainWindow)window).Width = 1086 * scale;
-                ((MainWindow)window).BaseCard.LayoutTransform = new ScaleTransform(scale, scale);
-            }
-
-            AppSettings.ApplicationSettings.SacredUtilsGuiScale = scale;
         }
 
         private static void OpenLink(string link)
---
+++ SacredUtils/resources/prp/ApplicationSettingsOneProperty.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using WPFSharp.Globalizer;
+
+namespace SacredUtils.resources.prp
+{
+    public class ApplicationSettingsOneProperty
+    {
+        public int Language
+        {
+            get
+            {
+                if (AppSettings.ApplicationSettings.AppUiLanguage == "based on system")
+                {
+                    return CultureInfo.InstalledUICulture.TwoLetterISOLanguageName == "ru" ? 0 : 1;
+                }
+
+                return AppSettings.ApplicationSettings.AppUiLanguage == "ru" ? 0 : 1;
+            }
+
+            set
+            {
+                if (value == 0)
+                {
+                    AppSettings.ApplicationSettings.AppUiLanguage = "ru";
+
+                    GlobalizedApplication.Instance.GlobalizationManager.SwitchLanguage("ru-RU", true);
+
+                    AppLogger.Log.Info("SacredUtils application language changed state to ru by user");
+                }
+
+                if (value == 1)
+                {
+                    AppSettings.ApplicationSettings.AppUiLanguage = "en";
+
+                    GlobalizedApplication.Instance.GlobalizationManager.SwitchLanguage("en-US", true);
+
+                    AppLogger.Log.Info("SacredUtils application language changed state to en by user");
+                }
+            }
+        }
+        
+        public int Theme
+        {
+            get => AppSettings.ApplicationSettings.ColorTheme == "light" ? 0 : 1;
+
+            set
+            {
+                if (value == 0)
+                {
+                    AppSettings.ApplicationSettings.ColorTheme = "light";
+
+                    GlobalizedApplication.Instance.StyleManager.SwitchStyle("Light.xaml");
+
+                    AppLogger.Log.Info("SacredUtils application theme changed state to light by user");
+                }
+
+                if (value == 1)
+                {
+                    AppSettings.ApplicationSettings.ColorTheme = "dark";
+
+                    GlobalizedApplication.Instance.StyleManager.SwitchStyle("Dark.xaml");
+
+                    AppLogger.Log.Info("SacredUtils application theme changed state to dark by user");
+                }
+            }
+        }
+
+        public int StartParams
+        {
+            get => AppSettings.ApplicationSettings.SacredStartArgs;
+
+            set
+            {
+                AppSettings.ApplicationSettings.SacredStartArgs = value;
+
+                AppLogger.Log.Info($"Sacred game startup params changed to {value} by user");
+            }
+        }
+
+        public string UiScale
+        {
+            get => $"{Convert.ToInt32(AppSettings.ApplicationSettings.SacredUtilsGuiScale * 100)}%";
+
+            set
+            {
+                if (value == "100%")
+                {
+                    ChangeScale(1.0); AppLogger.Log.Info("SacredUtils Ui scale changed state to 100% by user");
+                }
+
+                if (value == "105%")
+                {
+                    ChangeScale(1.05); AppLogger.Log.Info("SacredUtils Ui scale changed state to 105% by user");
+                }
+
+                if (value == "110%")
+                {
+                    ChangeScale(1.10); AppLogger.Log.Info("SacredUtils Ui scale changed state to 110% by user");
+                }
+
+                if (value == "115%")
+                {
+                    ChangeScale(1.15); AppLogger.Log.Info("SacredUtils Ui scale changed state to 115% by user");
+                }
+
+                if (value == "125%")
+                {
+                    ChangeScale(1.25); AppLogger.Log.Info("SacredUtils Ui scale changed state to 125% by user");
+                }
+
+                if (value == "150%")
+                {
+                    ChangeScale(1.50); AppLogger.Log.Info("SacredUtils Ui scale changed state to 150% by user");
+                }
+
+                if (value == "175%")
+                {
+                    ChangeScale(1.75); AppLogger.Log.Info("SacredUtils Ui scale changed state to 175% by user");
+                }
+
+                if (value == "200%")
+                {
+                    ChangeScale(2.0); AppLogger.Log.Info("SacredUtils Ui scale changed state to 200% by user");
+                }
+            }
+        }
+
+        private static void ChangeScale(double scale)
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                ((MainWindow)window).Height = 720 * scale;
+                ((MainWindow)window).Width = 1086 * scale;
+                ((MainWindow)window).BaseCard.LayoutTransform = new ScaleTransform(scale, scale);
+            }
+
+            AppSettings.ApplicationSettings.SacredUtilsGuiScale = scale;
+        }
+    }
+}
