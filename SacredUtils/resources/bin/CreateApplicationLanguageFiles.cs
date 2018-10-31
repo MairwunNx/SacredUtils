380b79324af2483db9305a7dc779ef04679c9d5a
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-10-31 17:42:44 +0300

    Improved code for creating theme and language files.

--- SacredUtils/resources/bin/CreateApplicationLanguageFiles.cs
+++ SacredUtils/resources/bin/CreateApplicationLanguageFiles.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using static SacredUtils.AppLogger;
 
 namespace SacredUtils.resources.bin
@@ -8,23 +7,10 @@ namespace SacredUtils.resources.bin
     {
         public static void Create()
         {
-            try
-            {
-                File.WriteAllBytes("$SacredUtils\\lang\\ru-RU\\ru-RU.xaml", Properties.Resources.ru_RU);
-                File.WriteAllBytes("$SacredUtils\\lang\\en-US\\en-US.xaml", Properties.Resources.en_US);
+            File.WriteAllBytes("$SacredUtils\\lang\\ru-RU\\ru-RU.xaml", Properties.Resources.ru_RU);
+            File.WriteAllBytes("$SacredUtils\\lang\\en-US\\en-US.xaml", Properties.Resources.en_US);
 
-                Log.Info("SacredUtils language files was successfully re-created!");
-            }
-            catch (Exception exception)
-            {
-                Log.Fatal("Creating SacredUtils language files done with fatal level error!!!");
-
-                Log.Fatal(exception.ToString);
-
-                Log.Info("Shutting down SacredUtils configurator ...");
-
-                Environment.Exit(0);
-            }
+            Log.Info("SacredUtils language files was successfully re-created!");
         }
     }
 }
--- SacredUtils/resources/bin/CreateApplicationThemeFiles.cs
+++ SacredUtils/resources/bin/CreateApplicationThemeFiles.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using static SacredUtils.AppLogger;
 
 namespace SacredUtils.resources.bin
@@ -8,23 +7,10 @@ namespace SacredUtils.resources.bin
     {
         public static void Create()
         {
-            try
-            {
-                File.WriteAllBytes("$SacredUtils\\thms\\light.xaml", Properties.Resources.Light);
-                File.WriteAllBytes("$SacredUtils\\thms\\dark.xaml", Properties.Resources.Dark);
+            File.WriteAllBytes("$SacredUtils\\thms\\light.xaml", Properties.Resources.Light);
+            File.WriteAllBytes("$SacredUtils\\thms\\dark.xaml", Properties.Resources.Dark);
 
-                Log.Info("SacredUtils theme files was successfully re-created!");
-            }
-            catch (Exception exception)
-            {
-                Log.Fatal("Creating SacredUtils theme files done with fatal level error!!!");
-                
-                Log.Fatal(exception.ToString);
-
-                Log.Info("Shutting down SacredUtils configurator ...");
-
-                Environment.Exit(0);
-            }
+            Log.Info("SacredUtils theme files was successfully re-created!");
         }
     }
 }
