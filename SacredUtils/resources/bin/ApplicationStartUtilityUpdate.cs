80d98b5a1755908778b62eabdbc85062e7ef98fe
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-06 22:58:56 +0300

    Improved code for ApplicationStartUtilityUpdate.

--- SacredUtils/resources/bin/ApplicationStartUtilityUpdate.cs
+++ SacredUtils/resources/bin/ApplicationStartUtilityUpdate.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
-using static SacredUtils.AppLogger;
 
 namespace SacredUtils.resources.bin
 {
@@ -9,20 +7,13 @@ namespace SacredUtils.resources.bin
     {
         public static void Start()
         {
-            try
-            {
-                Directory.CreateDirectory("$SacredUtils\\temp");
+            Directory.CreateDirectory("$SacredUtils\\temp");
 
-                File.Create("$SacredUtils\\temp\\updated.su");
+            File.Create("$SacredUtils\\temp\\updated.su");
 
-                Process.Start("mnxupdater.exe", AppDomain.CurrentDomain.FriendlyName + " _newVersionSacredUtilsTemp.exe");
+            Process.Start("mnxupdater.exe", AppSummary.CurrentExe + " _newVersionSacredUtilsTemp.exe");
 
-                ApplicationBaseWindowShutdown.Shutdown();
-            }
-            catch (Exception ex)
-            {
-                Log.Info(ex.ToString);
-            }
+            ApplicationBaseWindowShutdown.Shutdown();
         }
     }
 }
