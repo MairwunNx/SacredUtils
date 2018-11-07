7fb86db4c687cad68848f9f7bf55a5d4b68f8096
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-07 17:39:02 +0300

    Improved code for GamePlaySettingsThreeProperty.

--- SacredUtils/resources/prp/GamePlaySettingsThreeProperty.cs
+++ SacredUtils/resources/prp/GamePlaySettingsThreeProperty.cs
@@ -1,7 +1,5 @@
 ﻿using SacredUtils.resources.bin;
-using System.IO;
-using System.Linq;
-using System.Text;
+using System;
 
 namespace SacredUtils.resources.prp
 {
@@ -9,64 +7,28 @@ namespace SacredUtils.resources.prp
     {
         public bool CombineSlots
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "COMBINE_SLOTS : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("COMBINE_SLOTS : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("COMBINE_SLOTS", value ? 1 : 0);
         }
 
         public bool AcceptLicense
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "ACCEPT_LICENSE : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("ACCEPT_LICENSE : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("ACCEPT_LICENSE", value ? 1 : 0);
         }
 
         public bool FirstEnter
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "FIRST_LOGIN : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("FIRST_LOGIN : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("FIRST_LOGIN", value ? 1 : 0);
         }
 
         public bool Earthquake
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "SCREEN_QUAKE : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("SCREEN_QUAKE : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("SCREEN_QUAKE", value ? 1 : 0);
         }
