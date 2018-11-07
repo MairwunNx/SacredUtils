930743f2714278d4a96009502fbf6bed2220631a
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-07 17:32:26 +0300

    Improved code for GameGraphicsSettingsThreeProperty.

--- SacredUtils/resources/prp/GameGraphicsSettingsThreeProperty.cs
+++ SacredUtils/resources/prp/GameGraphicsSettingsThreeProperty.cs
@@ -1,7 +1,5 @@
 ﻿using SacredUtils.resources.bin;
-using System.IO;
-using System.Linq;
-using System.Text;
+using System;
 
 namespace SacredUtils.resources.prp
 {
@@ -9,80 +7,35 @@ namespace SacredUtils.resources.prp
     {
         public bool ForceBlackShadow
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "FORCE_BLACK_SHADOW : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("FORCE_BLACK_SHADOW : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("FORCE_BLACK_SHADOW", value ? 1 : 0);
         }
 
         public bool CompatVideo
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "COMPAT_VIDEO : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("COMPAT_VIDEO : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("COMPAT_VIDEO", value ? 1 : 0);
         }
 
         public bool FontAa
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "FONTAA : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("FONTAA : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("FONTAA", value ? 1 : 0);
         }
 
         public bool FullScreenAa
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "FSAA_FILTER : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("FSAA_FILTER : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("FSAA_FILTER", value ? 1 : 0);
         }
 
         public bool FontFilter
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "FONTFILTER : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("FONTFILTER : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("FONTFILTER", value ? 1 : 0);
         }
