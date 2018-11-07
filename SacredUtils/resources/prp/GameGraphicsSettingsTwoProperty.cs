7f971abb1772cf464effdd5586bc0bd3c5657a99
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-07 17:30:26 +0300

    Improved code for GameGraphicsSettingsTwoProperty.

--- SacredUtils/resources/prp/GameGraphicsSettingsTwoProperty.cs
+++ SacredUtils/resources/prp/GameGraphicsSettingsTwoProperty.cs
@@ -1,7 +1,5 @@
 ﻿using SacredUtils.resources.bin;
-using System.IO;
-using System.Linq;
-using System.Text;
+using System;
 
 namespace SacredUtils.resources.prp
 {
@@ -9,128 +7,56 @@ namespace SacredUtils.resources.prp
     {
         public bool RestrictionMemory
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "GFX_LIMIT128 : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("GFX_LIMIT128 : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("GFX_LIMIT128", value ? 1 : 0);
         }
 
         public bool ColorDepth
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "GFX32 : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("GFX32 : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("GFX32", value ? 1 : 0);
         }
 
         public bool PickupAnim
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "PICKUPANIM : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("PICKUPANIM : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("PICKUPANIM", value ? 1 : 0);
         }
 
         public bool Violence
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "VIOLENCE : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("VIOLENCE : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("VIOLENCE", value ? 1 : 0);
         }
 
         public bool FullScreen
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "FULLSCREEN : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("FULLSCREEN : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("FULLSCREEN", value ? 1 : 0);
         }
 
         public bool FullScreenAa
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "FSAA_FILTER : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("FSAA_FILTER : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("FSAA_FILTER", value ? 1 : 0);
         }
 
         public bool ShowMovie
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "SHOWMOVIE : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("SHOWMOVIE : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("SHOWMOVIE", value ? 1 : 0);
         }
 
         public bool ShowFinalMovie
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "SHOWEXTRO : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("SHOWEXTRO : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("SHOWEXTRO", value ? 1 : 0);
         }
