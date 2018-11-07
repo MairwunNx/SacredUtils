9ccaf70adb8028e0d3c18b6ef64e3e6b57ad3f4a
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-07 17:08:53 +0300

    Improved code for Sacred game chat settings.
    Also fixed bug with nullable language, if language not set in settings.

---
+++ SacredUtils/resources/bin/GetSacredGameInstalledLang.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+using System.IO;
+
+namespace SacredUtils.resources.bin
+{
+    public static class GetSacredGameInstalledLang
+    {
+        private static int Get()
+        {
+            int index = 1;
+
+            if (Directory.Exists("scripts"))
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo("scripts");
+                DirectoryInfo[] dirs = directoryInfo.GetDirectories();
+
+                if (dirs.Length > 1)
+                {
+                    switch (CultureInfo.InstalledUICulture.TwoLetterISOLanguageName)
+                    {
+                        case "ru" when Directory.Exists("scripts\\ru"): index = 0; break;
+                        case "en" when Directory.Exists("scripts\\us"): index = 1; break;
+                        case "us" when Directory.Exists("scripts\\us"): index = 1; break;
+                        case "de" when Directory.Exists("scripts\\de"): index = 2; break;
+                        case "sp" when Directory.Exists("scripts\\sp"): index = 3; break;
+                        case "fr" when Directory.Exists("scripts\\fr"): index = 4; break;
+                        default: index = 1; break;
+                    }
+                }
+                else if (dirs.Length == 1)
+                {
+                    if (Directory.Exists("scripts\\ru")) { index = 0; }
+                    if (Directory.Exists("scripts\\us")) { index = 1; }
+                    if (Directory.Exists("scripts\\de")) { index = 2; }
+                    if (Directory.Exists("scripts\\sp")) { index = 3; }
+                    if (Directory.Exists("scripts\\fr")) { index = 4; }
+                }
+            }
+
+            return index;
+        }
+
+        public static int GetLanguage()
+        {
+            int index = 5;
+
+            string language = GetSacredGameSettingValue.Get("LANGUAGE : ", false, true).ToString();
+
+            switch (language)
+            {
+                case "RU": index = 0; break;
+                case "US": index = 1; break;
+                case "DE": index = 2; break;
+                case "SP": index = 3; break;
+                case "FR": index = 4; break;
+            }
+
+            if (index == 5) { index = Get(); }
+
+            return index;
+        }
+    }
+}
---
+++ SacredUtils/resources/bin/GetSacredGameSettingValue.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using static SacredUtils.AppLogger;
+
+namespace SacredUtils.resources.bin
+{
+    public static class GetSacredGameSettingValue
+    {
+        public static object Get(string prefix, bool isBool, bool isLanguage)
+        {
+            string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
+
+            string line = text.FirstOrDefault(x => x.StartsWith(prefix));
+
+            Log.Info($"Loading Sacred game setting value for {prefix.Split(':')[0].Replace(" ", "")} ...");
+
+            if (isBool) { return line?.Substring(prefix.Length) == "1"; }
+
+            if (isLanguage)
+            { 
+                if (line != null) { return line.Substring(prefix.Length); }
+
+                return "NULL";
+            }
+
+            return line?.Substring(prefix.Length);
+        }
+    }
+}
