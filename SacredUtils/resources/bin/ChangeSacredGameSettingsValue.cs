038d722e2f6c0ace887288412aea2bc04d6b16b0
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-07 23:19:12 +0300

    Improved and optimized code for ChangeSettingFontValue.

--- SacredUtils/resources/bin/ChangeSacredGameSettingsValue.cs
+++ SacredUtils/resources/bin/ChangeSacredGameSettingsValue.cs
@@ -48,156 +48,29 @@ namespace SacredUtils.resources.bin
             {
                 string[] sizes = AppSettings.ApplicationSettings.SacredFontSizeArray.Split('|');
 
-                if (!File.ReadAllText(SacredConfigFile, Encoding.ASCII).Contains("FONT : 1, \""))
+                for (var ii = 0; ii < 8; ii++)
                 {
-                    using (StreamWriter file = new StreamWriter(SacredConfigFile, true, Encoding.ASCII))
-                    {
-                        file.WriteLine($"FONT : 1, \"{font}\", {sizes[0]}");
-                    }
-                }
-                else
-                {
-                    string[] text1 = File.ReadAllLines(SacredConfigFile, Encoding.ASCII);
+                    int fontSettings = ii + 1;
 
-                    for (int i = 0; i < text1.Length; i++)
+                    if (File.ReadAllText(SacredConfigFile, Encoding.ASCII).Contains($"FONT : {fontSettings}, \""))
                     {
-                        if (text1[i].Contains("FONT : 1, \""))
+                        string[] text1 = File.ReadAllLines(SacredConfigFile, Encoding.ASCII);
+
+                        for (int i = 0; i < text1.Length; i++)
                         {
-                            text1[i] = $"FONT : 1, \"{font}\", {sizes[0]}";
+                            if (text1[i].Contains($"FONT : {fontSettings}, \""))
+                            {
+                                text1[i] = $"FONT : {fontSettings}, \"{font}\", {sizes[ii]}";
 
-                            File.WriteAllLines(SacredConfigFile, text1);
+                                File.WriteAllLines(SacredConfigFile, text1);
+                            }
                         }
                     }
-                }
-
-                if (!File.ReadAllText(SacredConfigFile, Encoding.ASCII).Contains("FONT : 2, \""))
-                {
-                    using (StreamWriter file = new StreamWriter(SacredConfigFile, true, Encoding.ASCII))
+                    else
                     {
-                        file.WriteLine($"FONT : 2, \"{font}\", {sizes[1]}");
-                    }
-                }
-                else
-                {
-                    string[] text1 = File.ReadAllLines(SacredConfigFile, Encoding.ASCII);
-
-                    for (int i = 0; i < text1.Length; i++)
-                    {
-                        if (text1[i].Contains("FONT : 2, \""))
+                        using (StreamWriter file = new StreamWriter(SacredConfigFile, true, Encoding.ASCII))
                         {
-                            text1[i] = $"FONT : 2, \"{font}\", {sizes[1]}";
-
-                            File.WriteAllLines(SacredConfigFile, text1);
-                        }
-                    }
-                }
-
-                if (!File.ReadAllText(SacredConfigFile, Encoding.ASCII).Contains("FONT : 3, \""))
-                {
-                    using (StreamWriter file = new StreamWriter(SacredConfigFile, true, Encoding.ASCII))
-                    {
-                        file.WriteLine($"FONT : 3, \"{font}\", {sizes[2]}");
-                    }
-                }
-                else
-                {
-                    string[] text1 = File.ReadAllLines(SacredConfigFile, Encoding.ASCII);
-
-                    for (int i = 0; i < text1.Length; i++)
-                    {
-                        if (text1[i].Contains("FONT : 3, \""))
-                        {
-                            text1[i] = $"FONT : 3, \"{font}\", {sizes[2]}";
-
-                            File.WriteAllLines(SacredConfigFile, text1);
-                        }
-                    }
-                }
-
-                if (!File.ReadAllText(SacredConfigFile, Encoding.ASCII).Contains("FONT : 4, \""))
-                {
-                    using (StreamWriter file = new StreamWriter(SacredConfigFile, true, Encoding.ASCII))
-                    {
-                        file.WriteLine($"FONT : 4, \"{font}\", {sizes[3]}");
-                    }
-                }
-                else
-                {
-                    string[] text1 = File.ReadAllLines(SacredConfigFile, Encoding.ASCII);
-
-                    for (int i = 0; i < text1.Length; i++)
-                    {
-                        if (text1[i].Contains("FONT : 4, \""))
-                        {
-                            text1[i] = $"FONT : 4, \"{font}\", {sizes[3]}";
-
-                            File.WriteAllLines(SacredConfigFile, text1);
-                        }
-                    }
-                }
-
-                if (!File.ReadAllText(SacredConfigFile, Encoding.ASCII).Contains("FONT : 5, \""))
-                {
-                    using (StreamWriter file = new StreamWriter(SacredConfigFile, true, Encoding.ASCII))
-                    {
-                        file.WriteLine($"FONT : 5, \"{font}\", {sizes[4]}");
-                    }
-                }
-                else
-                {
-                    string[] text1 = File.ReadAllLines(SacredConfigFile, Encoding.ASCII);
-
-                    for (int i = 0; i < text1.Length; i++)
-                    {
-                        if (text1[i].Contains("FONT : 5, \""))
-                        {
-                            text1[i] = $"FONT : 5, \"{font}\", {sizes[4]}";
-
-                            File.WriteAllLines(SacredConfigFile, text1);
-                        }
-                    }
-                }
-
-                if (!File.ReadAllText(SacredConfigFile, Encoding.ASCII).Contains("FONT : 6, \""))
-                {
-                    using (StreamWriter file = new StreamWriter(SacredConfigFile, true, Encoding.ASCII))
-                    {
-                        file.WriteLine($"FONT : 6, \"{font}\", {sizes[5]}");
-                    }
-                }
-                else
-                {
-                    string[] text1 = File.ReadAllLines(SacredConfigFile, Encoding.ASCII);
-
-                    for (int i = 0; i < text1.Length; i++)
-                    {
-                        if (text1[i].Contains("FONT : 6, \""))
-                        {
-                            text1[i] = $"FONT : 6, \"{font}\", {sizes[5]}";
-
-                            File.WriteAllLines(SacredConfigFile, text1);
-                        }
-                    }
-                }
-
-                if (!File.ReadAllText(SacredConfigFile, Encoding.ASCII).Contains("FONT : 7, \""))
-                {
-                    using (StreamWriter file = new StreamWriter(SacredConfigFile, true, Encoding.ASCII))
-                    {
-                        file.WriteLine($"FONT : 7, \"{font}\", {sizes[6]}");
-                    }
-                }
-                else
-                {
-                    string[] text1 = File.ReadAllLines(SacredConfigFile, Encoding.ASCII);
-
-                    for (int i = 0; i < text1.Length; i++)
-                    {
-                        if (text1[i].Contains("FONT : 7, \""))
-                        {
-                            text1[i] = $"FONT : 7, \"{font}\", {sizes[6]}";
-
-                            File.WriteAllLines(SacredConfigFile, text1);
+                            file.WriteLine($"FONT : {fontSettings}, \"{font}\", {sizes[ii]}");
                         }
                     }
                 }
