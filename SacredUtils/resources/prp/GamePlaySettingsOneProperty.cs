f2f8d9350a5f0ef97d471f15031c14b24bfc36e6
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-07 17:35:37 +0300

    Improved code for GamePlaySettingsOneProperty.

--- SacredUtils/resources/prp/GamePlaySettingsOneProperty.cs
+++ SacredUtils/resources/prp/GamePlaySettingsOneProperty.cs
@@ -1,8 +1,5 @@
 ﻿using SacredUtils.resources.bin;
 using System;
-using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace SacredUtils.resources.prp
 {
@@ -10,106 +7,35 @@ namespace SacredUtils.resources.prp
     {
         public int PickupMode
         {
-            get
-            {
-                int index = 0;
-
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "PICKUPAUTO : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                if (line != null)
-                {
-                    index = Convert.ToInt32(line.Substring(prefix.Length));
-                }
-
-                return index;
-            }
+            get => Convert.ToInt32(GetSacredGameSettingValue.Get("PICKUPAUTO : ", false, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("PICKUPAUTO", value);
         }
 
         public int ConnectionType
         {
-            get
-            {
-                int index = 0;
-
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "NETWORK_SPEEDSETTINGS : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                if (line != null)
-                {
-                    index = Convert.ToInt32(line.Substring(prefix.Length));
-                }
-
-                return index;
-            }
+            get => Convert.ToInt32(GetSacredGameSettingValue.Get("NETWORK_SPEEDSETTINGS : ", false, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("NETWORK_SPEEDSETTINGS", value);
         }
 
         public string PictureOnStartup
         {
-            get
-            {
-                string content = "";
-
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "GFXSTARTUP : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                if (line != null) { content = line.Substring(prefix.Length); }
-
-                return content;
-            }
+            get => GetSacredGameSettingValue.Get("GFXSTARTUP : ", false, false).ToString();
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("GFXSTARTUP", value);
         }
 
         public string PictureOnLoad
         {
-            get
-            {
-                string content = "";
-
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "GFXLOADING : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                if (line != null) { content = line.Substring(prefix.Length); }
-
-                return content;
-            }
+            get => GetSacredGameSettingValue.Get("GFXLOADING : ", false, false).ToString(); 
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("GFXLOADING", value);
         }
 
         public int WarningLevel
         {
-            get
-            {
-                int value = 0;
-
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "WARNING_LEVEL : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                if (line != null) { value = Convert.ToInt32(line.Substring(prefix.Length)); }
-
-                return value;
-            }
+            get => Convert.ToInt32(GetSacredGameSettingValue.Get("WARNING_LEVEL : ", false, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("WARNING_LEVEL", value);
         }
