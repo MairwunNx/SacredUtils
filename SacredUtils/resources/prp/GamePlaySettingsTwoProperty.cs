92ecc96bf748cb78cc33e943e2858c1ff4efc39e
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-07 17:37:57 +0300

    Improved code for GamePlaySettingsTwoProperty.

--- SacredUtils/resources/prp/GamePlaySettingsTwoProperty.cs
+++ SacredUtils/resources/prp/GamePlaySettingsTwoProperty.cs
@@ -1,7 +1,5 @@
 ﻿using SacredUtils.resources.bin;
-using System.IO;
-using System.Linq;
-using System.Text;
+using System;
 
 namespace SacredUtils.resources.prp
 {
@@ -9,128 +7,56 @@ namespace SacredUtils.resources.prp
     {
         public bool TrackEnemy
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "AUTOTRACKENEMY : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("AUTOTRACKENEMY : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("AUTOTRACKENEMY", value ? 1 : 0);
         }
 
         public bool AutoSave
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "AUTOSAVE : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("AUTOSAVE : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("AUTOSAVE", value ? 1 : 0);
         }
 
         public bool DefaultSkills
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "DEFAULT_SKILLS : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("DEFAULT_SKILLS : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("DEFAULT_SKILLS", value ? 1 : 0);
         }
 
         public bool DebugLog
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "LOGGING : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("LOGGING : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("LOGGING", value ? 1 : 0);
         }
 
         public bool DamageIcons
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "TASKBAR_ICONS : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("TASKBAR_ICONS : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("TASKBAR_ICONS", value ? 1 : 0);
         }
 
         public bool PotionBar
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "SHOWPOTIONS : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("SHOWPOTIONS : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("SHOWPOTIONS", value ? 1 : 0);
         }
 
         public bool PlayerInfo
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "SHOW_HEROINFO : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("SHOW_HEROINFO : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("SHOW_HEROINFO", value ? 1 : 0);
         }
 
         public bool EnemyInfo
         {
-            get
-            {
-                string[] text = File.ReadAllLines(AppSettings.ApplicationSettings.SacredConfigurationFile, Encoding.ASCII);
-
-                string prefix = "SHOW_ENEMYINFO : ";
-
-                string line = text.FirstOrDefault(x => x.StartsWith(prefix));
-
-                return line != null && line.Substring(prefix.Length) == "1";
-            }
+            get => Convert.ToBoolean(GetSacredGameSettingValue.Get("SHOW_ENEMYINFO : ", true, false));
 
             set => ChangeSacredGameSettingsValue.ChangeSettingValue("SHOW_ENEMYINFO", value ? 1 : 0);
         }
