85788e3591fd49301b891290db6a82ee5e56b4f8
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-11-22 16:39:42 +0300

    Added checking only on digits to font dialog sizes textbox.

--- SacredUtils/resources/dlg/ApplicationFontSizesDialog.xaml.cs
+++ SacredUtils/resources/dlg/ApplicationFontSizesDialog.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SacredUtils.resources.dlg
 {
@@ -13,10 +15,15 @@ namespace SacredUtils.resources.dlg
         {
             BaseSizeCategoryComboBox.SelectionChanged += LoadSize;
             BaseSizeValueTextBox.TextChanged += ChangeSize;
+            BaseSizeValueTextBox.PreviewTextInput += ValidateValue;
+
+            BaseSizeValueTextBox.IsEnabled = false;
         }
 
         private void LoadSize(object sender, SelectionChangedEventArgs e)
         {
+            if (BaseSizeValueTextBox.IsEnabled == false) { BaseSizeValueTextBox.IsEnabled = true; }
+
             ComboBox comboBox = sender as ComboBox;
 
             string[] sizes = AppSettings.ApplicationSettings.SacredFontSizeArray.Split('|');
@@ -28,11 +35,15 @@ namespace SacredUtils.resources.dlg
         {
             string[] sizes = AppSettings.ApplicationSettings.SacredFontSizeArray.Split('|');
 
+            BaseSizeValueTextBox.Text = BaseSizeValueTextBox.Text.Replace(" ", "");
+
             sizes[BaseSizeCategoryComboBox.SelectedIndex] = BaseSizeValueTextBox.Text;
 
             AppSettings.ApplicationSettings.SacredFontSizeArray = $"{sizes[0]}|{sizes[1]}|{sizes[2]}|{sizes[3]}|{sizes[4]}|{sizes[5]}|{sizes[6]}";
 
             MainWindow.FontStgOne.FontSizesTxBox.Text = AppSettings.ApplicationSettings.SacredFontSizeArray;
         }
+
+        private void ValidateValue(object sender, TextCompositionEventArgs e) => e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
     }
 }
