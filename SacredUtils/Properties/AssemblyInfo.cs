df1c9eab8ddf0376ec9fd79cc5d5229530b8a074
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2019-02-01 10:04:12 +0300

    Updated version info and build number.

--- SacredUtils/Properties/AssemblyInfo.cs
+++ SacredUtils/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.4.2")]
-[assembly: AssemblyFileVersion("1.2.4.2")]
+[assembly: AssemblyVersion("1.2.4.3")]
+[assembly: AssemblyFileVersion("1.2.4.3")]
 [assembly: Guid("C8CA8A80-CDC1-41B3-A597-DCCF76F65A31")]
 
