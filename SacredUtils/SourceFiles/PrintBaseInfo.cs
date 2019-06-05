6c7ecf79485d558188d3eea546f45c336e564f3f
Pavel Erokhin <MairwunNx@gmail.com>
2019-06-05 20:16:08 +0300

    Formatted PrintBaseInfo class.
    Signed-off-by: Pavel Erokhin <MairwunNx@gmail.com>

--- SacredUtils/SourceFiles/PrintBaseInfo.cs
+++ SacredUtils/SourceFiles/PrintBaseInfo.cs
@@ -32,18 +32,25 @@ namespace SacredUtils.SourceFiles
                 : "Current launched SacredUtils application is not fully trusted");
             Log.Info($"Version of the common language runtime {Environment.Version}");
             Log.Info(
-                $"Full version of the common language runtime {RuntimeInformation.FrameworkDescription}");
+                $"Full version of the common language runtime {RuntimeInformation.FrameworkDescription}"
+            );
             Log.Info(
-                $"OS version {Environment.OSVersion.VersionString} {RuntimeInformation.OSArchitecture} bit");
+                $"OS version {Environment.OSVersion.VersionString} {RuntimeInformation.OSArchitecture} bit"
+            );
             Log.Info(
-                $"Bitness of the current SacredUtils process {RuntimeInformation.ProcessArchitecture} bit");
+                $"Bitness of the current SacredUtils process {RuntimeInformation.ProcessArchitecture} bit"
+            );
             Log.Info(
-                $"Allocated memory for SacredUtils {Environment.WorkingSet / 1024 / 1024} MB or {Environment.WorkingSet / 1024} KB");
-            Log.Info($"Running by current user name profile {Environment.UserName}");
+                $"Allocated memory for SacredUtils {Environment.WorkingSet / 1024 / 1024} MB or {Environment.WorkingSet / 1024} KB"
+            );
+            Log.Info($"Running by current user name profile {Environment.UserName}"
+            );
             Log.Info(
-                $"SacredUtils current domain assemblies count {AppDomain.CurrentDomain.GetAssemblies().Length}");
+                $"SacredUtils current domain assemblies count {AppDomain.CurrentDomain.GetAssemblies().Length}"
+            );
             Log.Info(
-                $"Involved dirs ({InvolvedDirs.Length}) : {InvolvedDirs.ToNormalString()}");
+                $"Involved dirs ({InvolvedDirs.Length}) : {InvolvedDirs.ToNormalString()}"
+            );
             Log.Info("============================================================");
         }
     }
